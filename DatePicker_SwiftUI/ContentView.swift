652d54256cfaf4391a4b1a45eed320400d0f88d4
Toxumuharu <ring.o.awlright@gmail.com>
2021-01-26 22:46:41 +0900

    first commit

--- DatePicker_SwiftUI/ContentView.swift
+++ DatePicker_SwiftUI/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var date = Date()
+    @State private var isShowDatePickerView: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                DatePicker("Start Date",
+                           selection: $date,
+                           displayedComponents: [.date])
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .padding()
+
+                Text(date, style: .date) // December 8, 2020
+                Spacer()
+            }
+            .navigationTitle(Text("DatePicker"))
+            
+            // SwiftUI 2 function toolbar
+            // replaced NavigationBarItems
+            // can select .bottombar
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    Button(action: {
+//                        self.isShowDatePickerView.toggle()
+//                    }) {
+//                        Image(systemName: "calendar.badge.plus")
+//                    }
+//                }
+//            }
+        }
+        
     }
 }
 
