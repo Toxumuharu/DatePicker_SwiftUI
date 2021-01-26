1d8ce2fc92bfdf5ee823f32ce6f3bc9c32f0850f
Toxumuharu <ring.o.awlright@gmail.com>
2021-01-26 22:46:15 +0900

    Initial Commit

---
+++ DatePicker_SwiftUI/DatePicker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePicker_SwiftUIApp.swift
+//  DatePicker_SwiftUI
+//
+//  Created by Toxumuharu on 2021/01/26.
+//
+
+import SwiftUI
+
+@main
+struct DatePicker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
